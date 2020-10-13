1a5d78ca0e6594689f5a5725fbba7db4d088a271
SimCo92 <sim.92@hotmail.it>
2020-10-13 17:54:34 +0200

    Updates

--- precache-manifest.dd6da5cb6265bb7cf4224573d0ea34f5.js
+++ precache-manifest.dd6da5cb6265bb7cf4224573d0ea34f5.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = [
   {
-    "revision": "2c8ef73b0c6cf46fa379",
-    "url": "/me/static/css/main.a99bdc09.chunk.css"
+    "revision": "ed843762eed0a4946d48",
+    "url": "/me/static/css/main.a414e28b.chunk.css"
   },
   {
-    "revision": "2c8ef73b0c6cf46fa379",
-    "url": "/me/static/js/main.2c8ef73b.chunk.js"
+    "revision": "ed843762eed0a4946d48",
+    "url": "/me/static/js/main.ed843762.chunk.js"
   },
   {
     "revision": "1caf0789ee8fdb1f0b3c",
@@ -16,7 +16,7 @@ self.__precacheManifest = [
     "url": "/me/static/js/2.5ec21d73.chunk.js"
   },
   {
-    "revision": "31216872a2c0caa89aaf22ca73461b30",
+    "revision": "81f9b5a3939d2ce12f720ac5ea3456b9",
     "url": "/me/index.html"
   }
 ];
